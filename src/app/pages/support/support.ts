3a064baa025eb26cbac80ab7e534e261fc75171f
Marco Novelli <ma.novelli@almaviva.it>
2025-09-11 19:24:02 +0200

    commit pagina support

--- src/app/pages/support/support.ts
+++ src/app/pages/support/support.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-support',
-  imports: [],
   templateUrl: './support.html',
-  styleUrl: './support.scss'
+  imports: [
+    ReactiveFormsModule
+  ],
+  styleUrls: ['./support.scss']
 })
 export class Support {
+  supportForm: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.supportForm = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      oggetto: ['', Validators.required],
+      messaggio: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.supportForm.valid) {
+      console.log('Invio modulo:', this.supportForm.value);
+      alert('Messaggio inviato con successo!');
+      this.supportForm.reset();
+      this.submitted = false;
+    }
+  }
 }
